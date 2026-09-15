9eee33224b1dd59a5cef948f9eef7a017f7dd5af
learner0116-sys <learner0116@gmail.com>
2026-09-15 13:18:25 +0300

    Add files via upload

---
+++ model/enums/PaymentInputType.java
@@ -0,0 +1,9 @@
+package com.edfaaly.backend.model.enums;
+
+/**
+ * يوضح إزاي الراكب حدد السائق: مسح QR Code، أو إدخال كود السائق يدويًا (Driver ID / رقم اللوحة)
+ */
+public enum PaymentInputType {
+    QR_SCAN,
+    MANUAL_ID
+}
